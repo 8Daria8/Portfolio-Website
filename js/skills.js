87aa3c7a3ead2e88454406f37952832eba0231bf
8Daria8 <161815169+8Daria8@users.noreply.github.com>
2024-12-23 13:28:44 +0100

    Add files via upload

--- js/skills.js
+++ js/skills.js
@@ -1,1 +1,19 @@
+"use strict";
 
+function showInfo(skillId) {
+  var skillInfo = document.getElementById(skillId);
+
+ 
+  if (skillInfo.style.display === "block") {
+    skillInfo.style.display = "none";
+  } else {
+  
+    var allSkills = document.querySelectorAll(".skill-info");
+    allSkills.forEach(function (info) {
+      info.style.display = "none";
+    });
+
+
+    skillInfo.style.display = "block";
+  }
+}
